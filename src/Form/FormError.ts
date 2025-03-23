8364c6447012b057f5146282c24a70812781b8c6
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2025-03-23 15:26:26 +0800

    fix: fix incorrect folder name

--- src/Form/FormError.ts
+++ src/Form/FormError.ts
@@ -1,7 +1,7 @@
-import { KomomoError } from "../Utils/Errors.js";
+import { KomomoError } from "../Utils/Errors.js"
 
 /**
  * An error that is thrown when a form is not sent to a player.
  * - 当表单未发送给玩家时抛出的错误。
  */
-export const UnsentFormError = KomomoError.subclass("UnsentFormError");
+export const UnsentFormError = KomomoError.subclass("UnsentFormError")
